4d544174bb32156afdb86a1150e9eb0c989f76d0
Cryphean <cryphean@gmail.com>
2021-05-20 20:15:34 +0200

    Delete public directory

--- public/scripts/loader.js
+++
@@ -1,7 +0,0 @@
-window.on("DOMContentLoaded", () => {
-    $(".loader").parentNode.removeChild($(".loader"));
-    $("#loading-screen").style.opacity = 0;
-    setTimeout(() => {
-        hide($("#loading-screen"));
-    }, 1000);
-});
